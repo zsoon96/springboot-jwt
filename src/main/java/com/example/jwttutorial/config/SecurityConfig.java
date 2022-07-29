dde3fc421a13acb693e3f414e48d70210915fb8d
zsoon96 <pres0616@gmail.com>
2022-07-30 01:11:22 +0900

    jwt 관련 Security 추가 설정

--- src/main/java/com/example/jwttutorial/config/SecurityConfig.java
+++ src/main/java/com/example/jwttutorial/config/SecurityConfig.java
@@ -1,12 +1,41 @@
 package com.example.jwttutorial.config;
 
+import com.example.jwttutorial.jwt.JwtAccessDeniedHandler;
+import com.example.jwttutorial.jwt.JwtAuthenticationEntryPoint;
+import com.example.jwttutorial.jwt.JwtSecurityConfig;
+import com.example.jwttutorial.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+// @PreAuthorize 어노테이션을 메소드 단위로 추가하기 위해 적용
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig (
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     // h2 콘솔 및 favicon 관련 요청은 security 로직 무시
     @Override
@@ -22,8 +51,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() // 토큰을 사용하기 때문에
+
+                .exceptionHandling() // exception 핸들링 할 때, 우리가 만들었던 클래스 추가
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint) // 401
+                .accessDeniedHandler(jwtAccessDeniedHandler) // 403
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다는 설정
+
+                .and()
                 .authorizeRequests() // HttpServletRequest 사용하는 요청들에 대한 접근제한 설정
                 .antMatchers("/api/hello").permitAll() // 해당 api 접근은 인증없이 모두 허용
-                .anyRequest().authenticated(); // 이외 나머지 접근은 인증이 필요함
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                .anyRequest().authenticated() // 이외 나머지 접근은 인증이 필요함
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
     }
 }
